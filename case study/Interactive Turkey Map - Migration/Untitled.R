c18a38f000bccdaa8726077dfff75c41caf1870c
hakan turğay <turgayh@itu.edu.tr>
2019-12-14 13:47:41 +0300

    migration case study update

--- case study/Interactive Turkey Map - Migration/Untitled.R
+++ case study/Interactive Turkey Map - Migration/Untitled.R
@@ -7,11 +7,9 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                            value = range(quakes$mag), step = 0.1
-                ),
-                selectInput("city", "Color Scheme",tr[1]
-                ),
+
+                selectInput("city", "Color Scheme",mig$Month),
+                
                 checkboxInput("legend", "Show legend", TRUE)
   )
 )
@@ -19,32 +17,19 @@ ui <- bootstrapPage(
 server <- function(input, output, session) {
   
   filteredData <- reactive({
-    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    #quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    mig[mig$Month == input$city]
   })
   
 
 
   
   output$map <- renderLeaflet({
-    indis <- input$city == tr
-    leaflet(tr) %>% addTiles() %>%
-      addMarkers(~lng, ~lat, popup = ~htmlEscape(city))
+    mig = filteredData()
+    leaflet(mig) %>% addTiles() %>%
+      addMarkers(mig$Lng, mig$Lat, popup = ~htmlEscape(mig$Islands))
   })
   
-  # Use a separate observer to recreate the legend as needed.
-  observe({
-    proxy <- leafletProxy("map", data = tr)
-    
-    # Remove any existing legend, and only if the legend is
-    # enabled, create a new one.
-    proxy %>% clearControls()
-    if (input$legend) {
-      pal <- colorpal()
-      proxy %>% addLegend(position = "bottomright",
-                          pal = pal, values = ~mag
-      )
-    }
-  })
 }
 
 shinyApp(ui, server)
