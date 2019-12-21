745b33d17bdbbf7734c8e1f3ee25432b34bf8f4f
hakan <hakan@hakan-MacBook-Pro.local>
2019-12-21 14:24:00 +0300

    update Island Migration Case Study

--- case study/Interactive Turkey Map - Migration/app.R
+++
@@ -1,4 +0,0 @@
-library(maps)
-mapStates = map("state", fill = TRUE, plot = FALSE)
-leaflet(data = mapStates) %>% addTiles() %>%
-    addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
