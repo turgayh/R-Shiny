f8221dfc89e0fa365b7aef63d1e365c1ff414f6d
hakan <hakan@hakan-MacBook-Pro.local>
2019-12-22 01:41:51 +0300

    update case study and slides

--- case study/Interactive Turkey Map - Migration/IslandsMigration.R
+++ case study/Interactive Turkey Map - Migration/IslandsMigration.R
@@ -2,6 +2,10 @@ library(shiny)
 library(leaflet)
 library(RColorBrewer)
 library(htmltools)
+library(readxl)
+
+mig <- read_excel("islands_data_october-november.xlsx")
+View(mig)
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
@@ -28,8 +32,8 @@ server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     leaflet(mig) %>% addTiles() %>%
-      fitBounds(min_lng, min_lat, max_lng, max_lat)
-      #addMarkers(mig$Lng, mig$Lat, popup = ~htmlEscape(mig$Islands))
+      fitBounds(min_lng, min_lat, max_lng, max_lat)  %>%
+      addMarkers(mig$Lng, mig$Lat, popup = ~htmlEscape(mig$Islands))
   })
   
 
