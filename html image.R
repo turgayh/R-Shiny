067dc59abd8a1a463c50b2a19fe9044c3d3228d0
taylanbt <56889397+taylanbt@users.noreply.github.com>
2019-12-28 14:13:09 +0300

    images are added and improvements are made

---
+++ html image.R
@@ -0,0 +1,6 @@
+<div align ="center"; padding-bottom: 25px>
+  <img src="../images/reactivity result.PNG" alt="res" 
+  class="center" width="1000" height="400"> 
+    </div>
+    
+    style="width:512px;height:512px"
---
+++ ss_alınacak_kodlar/app.R
@@ -0,0 +1,9 @@
+ui <-
+    fluidPage (
+        wellPanel(
+            numericInput(inputId = 'n', 'Sample size', value = 25),
+            submitButton () ) , plotOutput(outputId = 'hist'))
+server <- function(input , output ){
+    
+} > 
+    shinyApp(ui = ui , server = server)
