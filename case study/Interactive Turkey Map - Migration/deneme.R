cdf700f6dfac89eedb9b0f9eb9c2b7c34e9f284e
hakan turğay <turgayh@itu.edu.tr>
2019-12-29 18:45:11 +0300

    Update deneme.R

--- case study/Interactive Turkey Map - Migration/deneme.R
+++ case study/Interactive Turkey Map - Migration/deneme.R
@@ -1,11 +1,50 @@
-library(shiny)
-
-
-ui <- fluidPage(
-  radioGroupButtons(inputId = "somevalue", choices = c("A", "B", "C")), verbatimTextOutput("value")
-)
-server <- function(input, output) {
-  output$value <- renderText({ input$somevalue }) }
-shinyApp(ui, server)
-
-
+if (interactive()) {
+  
+  library(shiny)
+  library(shinyWidgets)
+  
+  ui <- fluidPage(
+    tags$h1("Pretty switches"),
+    br(),
+    
+    fluidRow(
+      column(
+        width = 4,
+        prettySwitch(inputId = "switch1", label = "Default:"),
+        verbatimTextOutput(outputId = "res1"),
+        br(),
+        prettySwitch(inputId = "switch4",
+                     label = "Fill switch with status:",
+                     fill = TRUE, status = "primary"),
+        verbatimTextOutput(outputId = "res4")
+      ),
+      column(
+        width = 4,
+        prettySwitch(inputId = "switch2",
+                     label = "Danger status:",
+                     status = "danger"),
+        verbatimTextOutput(outputId = "res2")
+      ),
+      column(
+        width = 4,
+        prettySwitch(inputId = "switch3",
+                     label = "Slim switch:",
+                     slim = TRUE),
+        verbatimTextOutput(outputId = "res3")
+      )
+    )
+    
+  )
+  
+  server <- function(input, output, session) {
+    
+    output$res1 <- renderPrint(input$switch1)
+    output$res2 <- renderPrint(input$switch2)
+    output$res3 <- renderPrint(input$switch3)
+    output$res4 <- renderPrint(input$switch4)
+    
+  }
+  
+  shinyApp(ui, server)
+  
+}
